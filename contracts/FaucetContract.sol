397feb574395923f6cbdf2b9a9fea411ae3e9f12
gerundel1 <germalikov@gmail.com>
2024-05-12 11:17:05 -0400

    Delete unused variables and make Faucet contract payable

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,5 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 contract Faucet {
-    //storage variables
-    uint public funds = 1000;
-    int public counter = -10;
-    uint32 public test = 4294967295;
+    receive() external payable {}
 }
